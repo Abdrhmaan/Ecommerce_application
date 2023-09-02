ab47ba9c1ba0efaad13b70be5a5ac81ff17848bf
XAAN-YRE <xaan087@gmail.com>
2023-09-02 04:04:42 -0700

    first commit

---
+++ FICIRCAMP/index.js
@@ -0,0 +1,63 @@
+console.log("j")
+
+console.log("hh")
+
+let xaan = "jirde"
+let cohort = 'ficircamp'
+ xaan = "ROOBLE FICKIRCMAP"
+
+ // kani  waaa calaamada litrial ama literals waaa isku darka javascpte iyo html
+ console.log(`iam xaan yre waxaan wax ka artaa' + ${cohort}  ${xaan}}`)
+
+
+
+
+ const leonel = 'ronaldo'
+
+
+
+let jirde = "xaan"
+console.log(leonel)
+console.log(jirde)
+
+
+// arimathic Operator
+let x = 2
+
+console.log(1+2)
+console.log(1-2)
+console.log(1/2)
+console.log(1%2)
+console.log(1**2)
+x++
+console.log(x)
+x--
+console.log(x)
+
+// comparism Opertor
+
+let xx = 10
+let y = 30
+
+console.log(x > y)
+console.log(x < y)
+console.log(x >= y)
+console.log(x <=y)
+console.log(x == y)
+console.log(x === y)
+
+
+
+// if statmente 
+&& || !=
+
+
+
+
+// arimathic Operator
+
+
+
+// string litreals 
+
+
